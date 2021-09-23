7272621ded1a8eafa4f55083ef771bfda8fee0f2
Esteban Torres <32805987+estebantorresa6@users.noreply.github.com>
2021-09-22 21:17:53 -0500

    Delete env.sh

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export PORT=4001
-export SECRET='secret'
-export MONGO_URI='mongodb+srv://Esteban:sfakn8i5@cluster0.vl18p.mongodb.net/AdoptaPet?retryWrites=true&w=majority'
